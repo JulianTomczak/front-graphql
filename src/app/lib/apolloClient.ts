ba2e19fa0fc56297efc111af3011bb9ff819d100
JulianTomczak <julian0graff0metal@gmail.com>
2025-09-12 17:41:56 -0300

    Update apolloClient.ts

--- src/app/lib/apolloClient.ts
+++ src/app/lib/apolloClient.ts
@@ -1,8 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const httpLink = new HttpLink({
-  //uri: 'http://localhost:3000/graphql',
-  uri:'http://localhost:5137/graphql/',
+  uri: 'http://localhost:3000/graphql',
 });
 
 const client = new ApolloClient({
